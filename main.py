485e2809b43481e778b902b1dd59ca7216105b60
Mitch <160351653+CallmeMitch@users.noreply.github.com>
2024-02-24 18:55:19 +0100

    Update main.py
    More readable

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import os
 import sys
 
 file = sys.argv[1]
-EXPECTED_NAME = ["fix", "feat", "docs"]
+EXPECTED_NAME = ["fix:", "feat:", "docs:"]
+
+def verif_global(git_verif):
+    if git_verif not in EXPECTED_NAME:
+        print("Check failed for", git_verif,"\nExemple ==> fix: description")
+        sys.exit(1)
+    print("Check passed for", git_verif)
+
 
 if os.path.isfile(file):
     openned_file = open(file, "r")
     openned_line_in_file = openned_file.readline()
-    check_word = openned_line_in_file.split()[0]
-    verif_global(check_word)
+    if len(openned_line_in_file) < 50:
+        check_word = openned_line_in_file.split()[0]
+        verif_global(check_word)
+    else:
+        print("50 characters or less")
+        sys.exit(1)
     openned_file.close()
-    exit()
-else:
-    print("expected argument:\ntype[optional scope]: description ==> fix: The probleme is fixed!\n[optional body]\n[optional footer(s)]")
-    print("The type can be fix, feat or docs(noon value)")
-
-
-def verif_global(git_verif):
-    if git_verif in EXPECTED_NAME:
-        print("Check pass")
-    
