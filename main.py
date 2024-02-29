e4c53299fd2bbcf4ef7e1622df20eced22916675
Mitch <160351653+CallmeMitch@users.noreply.github.com>
2024-02-29 11:49:25 +0100

    Update main.py
    feat: add fonction ;)

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ import sys
 file = sys.argv[1]
 EXPECTED_NAME = ["fix:", "feat:", "docs:"]
 
-def verif_global(git_verif):
+def verif_first_word_of_commit_message(git_verif):
     if git_verif not in EXPECTED_NAME:
         print("Check failed for", git_verif,"\nExemple ==> fix: description")
         sys.exit(1)
     print("Check passed for", git_verif)
 
-
-if os.path.isfile(file):
-    openned_file = open(file, "r")
-    openned_line_in_file = openned_file.readline()
+def open_file_for_check_commit():
+    if os.path.isfile(file):
+        openned_file = open(file, "r")
+        openned_line_in_file = openned_file.readline()
     if len(openned_line_in_file) < 50:
         check_word = openned_line_in_file.split()[0]
-        verif_global(check_word)
+        verif_first_word_of_commit_message(check_word)
     else:
         print("50 characters or less")
         sys.exit(1)
     openned_file.close()
+
+open_file_for_check_commit()
